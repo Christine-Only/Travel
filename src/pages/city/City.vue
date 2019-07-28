5f2bcfb703db5e2484913df5e1ea2aaea1f3a8eb
danya <tsy17321289030@163.com>
2019-07-28 22:48:51 +0800

    add list-alphabet

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,17 +2,23 @@
   <div class="city">
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/CityHeader'
 import CitySearch from './components/CitySearch'
+import CityList from './components/CityList'
+import CityAlphabet from './components/CityAlphabet'
 export default {
   name: 'City',
   components: {
     CityHeader,
-    CitySearch
+    CitySearch,
+    CityList,
+    CityAlphabet
   }
 }
 </script>
---
+++ src/pages/city/components/CityAlphabet.vue
@@ -0,0 +1,37 @@
+<template>
+  <ul class="list">
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+    <li class="item">A</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'CityAlphabet'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl';
+
+.list {
+  position: absolute;
+  right: 0;
+  top: 1.58rem;
+  bottom: 0;
+  width: 0.4rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  .item {
+    line-height: 0.4rem;
+    text-align: center;
+    color: $bgColor;
+  }
+}
+</style>
